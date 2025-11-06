841960b44b112ba93c7085928ebd723fca5393c6
andre-ortega <ortega.andre.j@gmail.com>
2025-11-05 21:19:08 -0800

    hello world

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const app = express()
+const port = process.env.PORT || 65433;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! :D')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
